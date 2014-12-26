71527ac56cacd48a38db7fae79a57adb25b9146e
Shoji Kunitomo <kakeru8@gamil.com>
2014-12-27 01:31:46 +0900

    Initial Commit that succeced to start my first app.

---
+++ README.md
@@ -0,0 +1,2 @@
+参考にしたサイト
+http://kozo002.blogspot.jp/2012/04/rubybundler-sinatra.html
---
+++ app.rb
@@ -0,0 +1,6 @@
+require 'rubygems' unless defined? ::Gem
+require 'sinatra'
+
+get '/' do
+  'Hello, World'
+end
